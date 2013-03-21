35740115c543e40decfa837f867de4706ef8f9bd
Phil <filliv89@gmail.com>
2013-03-21 16:42:19 +0100

    Ricomincio da qui

--- Host.py
+++ Host.py
@@ -21,27 +21,12 @@ class Host():
         for i in range(self.Num_Cores):
             self.core.append(Core.Core(10))
             print("Core creato n°",i)
-      #--------------------------------------------  
-    def find_comma(self,start):
-        print("start= ",start)
-        i=start
-        while i!=',':
-            i=i+1
-        return i
             
     def fill (self,percent):
-        
-        for i in range(1,self.Num_Cores):
-                  
-            comma=self.find_comma(i)
-            print("la virgola è ",comma)
-            i=comma+1
-        
-        
+        for c in range(self.Num_Cores):
+            print("Ciao",c)
             
-          
-                
-                
+                            
     def Run(self,timer):
         #while(True):
             percent=psutil.cpu_percent(interval=timer, percpu=True)
